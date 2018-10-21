905b8d235a8a23033191e634e2e8085159263445
DrDezmond <you@example.com>
2018-10-22 00:56:13 +0300

    added borders to cell component

--- src/components/cell.js
+++ src/components/cell.js
@@ -5,7 +5,13 @@ export class Cell extends PureComponent {
     const color = this.props.alive ? 'black' : 'white';
     return (
       <div
-        style={{ background: color, width: '10px', height: '10px', margin: 0 }}
+        style={{
+          background: color,
+          width: '10px',
+          height: '10px',
+          margin: 0,
+          border: '1px solid #e3e3e3'
+        }}
       />
     );
   }
